d5e4a5d04886d0825ba5f00249cac32036b9881d
undelubre <undelubre@gmail.com>
2019-06-16 21:49:22 +0200

    Se añade comentario

--- MProbas/src/ProbasBasicas/SizeNumbers.java
+++ MProbas/src/ProbasBasicas/SizeNumbers.java
@@ -5,7 +5,7 @@ public class SizeNumbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		//Max-Min values
 		System.out.println("Max Integer= "+Integer.MAX_VALUE+" - Min Integer= "+Integer.MIN_VALUE);
 		System.out.println("Max Long= "+Long.MAX_VALUE+" - Min Long= "+Long.MIN_VALUE);
 		System.out.println("Max Float= "+Float.MAX_VALUE+" - Min Float= "+Float.MIN_VALUE);
