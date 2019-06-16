290272b46d9d22f65a38fa0cad6a30c0a1f2c3a0
undelubre <undelubre@gmail.com>
2019-06-16 21:45:10 +0200

    Update SizeNumbers.java

--- MProbas/src/ProbasBasicas/SizeNumbers.java
+++ MProbas/src/ProbasBasicas/SizeNumbers.java
@@ -3,12 +3,13 @@ package ProbasBasicas;
 public class SizeNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub"
+		// TODO Auto-generated method stub
 		
 		
 		System.out.println("Max Integer= "+Integer.MAX_VALUE+" - Min Integer= "+Integer.MIN_VALUE);
 		System.out.println("Max Long= "+Long.MAX_VALUE+" - Min Long= "+Long.MIN_VALUE);
 		System.out.println("Max Float= "+Float.MAX_VALUE+" - Min Float= "+Float.MIN_VALUE);
+		
 	}
 
 }
