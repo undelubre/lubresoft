a4e60c8e9f0b54b11f7d381a6b25898fdf190fc8
unoh <unoh@unhaoh.gz>
2020-05-01 01:14:49 +0200

    Un comentario

--- MProbas/src/ProbasBasicas/SizeNumbers.java
+++ MProbas/src/ProbasBasicas/SizeNumbers.java
@@ -3,12 +3,11 @@ package ProbasBasicas;
 public class SizeNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub"
-		
-		
+		//Max-Min values
 		System.out.println("Max Integer= "+Integer.MAX_VALUE+" - Min Integer= "+Integer.MIN_VALUE);
 		System.out.println("Max Long= "+Long.MAX_VALUE+" - Min Long= "+Long.MIN_VALUE);
 		System.out.println("Max Float= "+Float.MAX_VALUE+" - Min Float= "+Float.MIN_VALUE);
+		
 	}
 
 }
