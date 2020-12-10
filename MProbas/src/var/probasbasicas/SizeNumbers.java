3bd554a9eaadda97baf90b18ec198ee8c8e7383b
manuelpp <manuelpp@ext.inditex.com>
2020-12-10 12:09:08 +0100

    Cambios menores

--- MProbas/src/var/probasbasicas/SizeNumbers.java
+++ MProbas/src/var/probasbasicas/SizeNumbers.java
@@ -7,6 +7,7 @@ public class SizeNumbers {
 		System.out.println("Max Integer= "+Integer.MAX_VALUE+" - Min Integer= "+Integer.MIN_VALUE);
 		System.out.println("Max Long= "+Long.MAX_VALUE+" - Min Long= "+Long.MIN_VALUE);
 		System.out.println("Max Float= "+Float.MAX_VALUE+" - Min Float= "+Float.MIN_VALUE);
+		System.out.println("Max Double= "+Double.MAX_VALUE+" - Min Double= "+Double.MIN_VALUE);
 		
 	}
 
